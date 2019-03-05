975216f938c926eb83822e4b072086f9068a2e46
linweizhong <linweizhong@meituan.com>
2019-03-05 16:18:43 +0800

    by luc

---
+++ src/main/java/com/luc/mudan/dp/composite/Client.java
@@ -0,0 +1,35 @@
+package com.luc.mudan.dp.composite;
+
+/**
+ * @author : luc
+ * @date : 2019-03-05 12:19
+ * Description:
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        System.out.println(">>>>>>>>>>>>>");
+        Component root = new Composite("Root");
+        root.add(new Leaf("Leaf A in Root"));
+        root.add(new Leaf("Leaf B in Root"));
+
+        Component branchX = new Composite("Branch X in Root");
+        Component branchY = new Composite("Branch Y in Root");
+        root.add(branchX);
+        root.add(branchY);
+
+        branchX.add(new Leaf("Leaf A in Branch X"));
+
+        // 为BranchX增加枝节点
+        Component branchZ = new Composite("Branch Z in Branch X");
+        branchX.add(branchZ);
+
+        // 为BranchY增加叶节点
+        branchY.add(new Leaf("Leaf in Branch Y"));
+
+        // 为BranchZ增加叶节点
+        branchZ.add(new Leaf("Leaf in Branch Z"));
+
+        root.display();
+    }
+}
---
+++ src/main/java/com/luc/mudan/dp/composite/Composite.java
@@ -0,0 +1,36 @@
+package com.luc.mudan.dp.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : luc
+ * @date : 2019-03-05 09:33
+ * Description:
+ */
+public class Composite extends Component {
+
+    public Composite(String name) {
+        super(name);
+    }
+
+    private List<Component> children = new ArrayList<>();
+
+    @Override
+    public void add(Component component) {
+        children.add(component);
+    }
+
+    @Override
+    public void remove(Component component) {
+        children.remove(component);
+    }
+
+    @Override
+    public void display() {
+        System.out.println(name);
+        for (Component component  : children) {
+            component.display();
+        }
+    }
+}
