45bab77b3a8eb300bf55adc2c7bd49d604b9f1df
linweizhong <linweizhong@meituan.com>
2019-03-18 20:01:25 +0800

    add decorator

---
+++ src/main/java/com/luc/mudan/dp/decorator/Client.java
@@ -0,0 +1,14 @@
+package com.luc.mudan.dp.decorator;
+
+/**
+ * @author : luc
+ * @date : 2019-03-18 19:59
+ * Description:
+ */
+public class Client {
+    public static void main(String[] args) {
+        Component component = new ConcreteComponent();
+        ConcreteDecorator concreteDecorator = new ConcreteDecorator(component);
+        concreteDecorator.doSomething();
+    }
+}
---
+++ src/main/java/com/luc/mudan/dp/decorator/Decorator.java
@@ -0,0 +1,19 @@
+package com.luc.mudan.dp.decorator;
+
+/**
+ * @author : luc
+ * @date : 2019-03-18 17:37
+ * Description:
+ */
+public class Decorator implements Component {
+    private Component component;
+
+    public Decorator(Component component) {
+        this.component = component;
+    }
+
+    @Override
+    public void doSomething() {
+        System.out.println("Decorator: I do ...");
+    }
+}
