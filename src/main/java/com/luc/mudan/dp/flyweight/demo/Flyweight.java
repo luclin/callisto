35ecef297ee673ba2126a5e1eaa1ea944ae6e32e
linweizhong <linweizhong@meituan.com>
2019-03-25 11:41:51 +0800

    add flyweight pattern

---
+++ src/main/java/com/luc/mudan/dp/flyweight/demo/Flyweight.java
@@ -0,0 +1,10 @@
+package com.luc.mudan.dp.flyweight.demo;
+
+/**
+ * @author : luc
+ * @date : 2019-03-25 10:35
+ * Description:
+ */
+public interface Flyweight {
+    void operation(String state);
+}
---
+++ src/main/java/com/luc/mudan/dp/flyweight/line/LineFlyweigt.java
@@ -0,0 +1,10 @@
+package com.luc.mudan.dp.flyweight.line;
+
+/**
+ * @author : luc
+ * @date : 2019-03-25 11:16
+ * Description:
+ */
+public interface LineFlyweigt {
+    void draw(String extrinsic);
+}
