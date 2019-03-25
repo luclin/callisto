8ceb6b93ad4a10023fd058095c104ec1342639c9
linweizhong <linweizhong@meituan.com>
2019-03-25 11:52:11 +0800

    add facade pattern

---
+++ src/main/java/com/luc/mudan/dp/facade/Disk.java
@@ -0,0 +1,16 @@
+package com.luc.mudan.dp.facade;
+
+/**
+ * @author : luc
+ * @date : 2019-03-25 11:47
+ * Description:
+ */
+public class Disk {
+    void start() {
+        System.out.println("Disk start ...");
+    }
+
+    void shutdown() {
+        System.out.println("Disk shutdown ...");
+    }
+}
